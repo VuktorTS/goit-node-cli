b3daa83202a212dff6e747dcb49f900ae491fd66
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2024-02-23 20:46:51 +0200

    add function allContars

--- contacts.js
+++ contacts.js
@@ -1,5 +1,13 @@
 import fs from "fs/promises";
 import path from "path";
+import { nanoid } from "nanoid";
 
-const contactPath = path.resolve("src", "db", "contacts.json");
-console.log("contactPath: ", contactPath);
+const contactPath = path.resolve("db", "contacts.json");
+
+export const listContacts = async () => {
+  const contacts = await fs.readFile(contactPath);
+  return JSON.parse(contacts);
+};
+export const getContactById = async (contactId) => {};
+export const removeContact = async (contactId) => {};
+export const addContact = async (name, email, phone) => {};
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
   .option("-n, --name <type>", "user name")
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
-
+// node index.js -a list
+// node index.js -a get -i 05olLMgyVQdWRwgKfg5J6
+// node index.js -a add -n Mango -e mango @gmail.com -p 322 - 22 - 22
+// node index.js -a remove -i qdggE76Jtbfd9eWJHrssH
 program.parse();
 
 const options = program.opts();
@@ -14,7 +24,8 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const allContacts = await listContacts();
+      return console.table(allContacts);
       break;
 
     case "get":
